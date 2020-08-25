5bed5f9980870e0f5887801b99e7239ff968e8a4
yikolden <ljh34210329@gmail.com>
2020-08-25 20:44:01 +0900

    구글 시트 연도에 따라 시트에 넣도록 수정

--- php/insertPresentConsult.php
+++ php/insertPresentConsult.php
@@ -53,24 +53,26 @@ if (count($list) > 0)
 }
 
 $mailSender = new MailSender();
-$result = $mailSender->SendTo("sobang@millcompany.co.kr", "명절선물 상담예약", "신규 명절선물 상담예약이 접수되었습니다 :)", array($filepath));
+$emailResult = $mailSender->SendTo("sobang@millcompany.co.kr", "명절선물 상담예약", "신규 명절선물 상담예약이 접수되었습니다 :)", array($filepath));
 
-if ($result == FALSE) 
+if ($emailResult == FALSE) 
 {
-    $message = MakeMessage(FALSE, "이메일 발송 실패", $result->GetError());
+    $message = MakeMessage(FALSE, "이메일 발송 실패", $mailSender->GetError());
     Response(500, $message);
     $conn->close();
     exit();
 }
 
 $messageSender = new MessageSender();
-$result = $messageSender->SendMessage("신규 명절선물 상담예약이 접수되었습니다 :)", "01047210778", /* Test Mode */"N");
+$messageResult = $messageSender->SendMessage("신규 명절선물 상담예약이 접수되었습니다 :)", "01047210778", /* Test Mode */"N");
+array_push($result, array("messageResult" => $messageResult));
 
 if ($lastListData != null) {
     $sheetWriter = new SheetWriter();
     $data = array(
         "type" => "present",
         "header_row" => "2",
+        "year" => date("Y"),
         "색인" => $lastListData["index"],
         "단체명" => $lastListData["organization"],
         "담당자" => $lastListData["personInCharge"],
