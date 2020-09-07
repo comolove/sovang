20e8f6027ff0eabc018cc37a64185f747517ff2f
yikolden <ljh34210329@gmail.com>
2020-09-07 19:00:54 +0900

    애널리틱스 적용

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,18 @@ import "aos/dist/aos.css";
 import VueCookies from "vue-cookies";
 import VueScrollTo from "vue-scrollto";
 
+import VueGtag from "vue-gtag";
+
 Vue.config.productionTip = false;
 
 Vue.use(VueCookies);
 Vue.use(VueScrollTo);
+Vue.use(VueGtag, {
+  config: { 
+    id: "UA-38304686-2" 
+  }
+});
+
 
 new Vue({
   created() {
